4bf3241d1ea3d3bd6e5f66ff68161d1c33214b21
tulians <julianailan@outlook.com>
2015-01-05 16:01:21 -0300

    Minor visual modifications on subject's new & edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users #, :controllers => { registrations: 'registrations' }
   resources :comments
-
+  resources :users
+  resources :subjects
+  
   root :to => redirect('/subjects')
 
   get 'pages/info'
 
-  resources :subjects
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
