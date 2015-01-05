8b16e12e139b560665962692a721efe7115386ec
tulians <julianailan@outlook.com>
2015-01-05 19:55:30 -0300

    More minor visual modifications added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users #, :controllers => { registrations: 'registrations' }
+  devise_for :users 
   resources :comments
   resources :users
   resources :subjects
