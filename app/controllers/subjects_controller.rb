79c9c29e5a895bfc38c7affb936acfbb57060a69
tulians <julianailan@outlook.com>
2015-01-21 15:28:02 -0300

    Changes in font color

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -72,6 +72,6 @@ class SubjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def subject_params
-      params.require(:subject).permit(:name, :description, :picture, :code, :year, :int_ord, :name, :surname)
+      params.require(:subject).permit(:name, :description, :picture, :code, :year, :int_ord, :website)
     end
 end
