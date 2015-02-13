3406e0a457c68816012f85ca1ba54de82426e61d
lucashour <lucashour1993@gmail.com>
2015-02-13 18:36:33 -0300

    Minor visual details added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,6 +17,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
  
   def account_update_params
-    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
   end
 end
