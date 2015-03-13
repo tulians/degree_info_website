5876d17ce48b26abf1f971c06a969ac3de0365ac
Lucas Hourquebie <lucashour1993@gmail.com>
2015-03-13 16:23:01 -0300

    Flash[:notice] added in file uploading

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -8,6 +8,7 @@ class UploadsController < ApplicationController
 			params[:upload][:file].each do |up|
 				@upload = Upload.create(:file=>up)
 				@upload.save
+				flash[:notice] = "Archivo/s subido/s exitosamente. Quedará/n para verificación, luego serán publicados en el sitio web."
 			end
 		else
 			flash[:alert] = "No seleccionaste archivos."
