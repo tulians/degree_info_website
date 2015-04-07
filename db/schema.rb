5e48ba503f888cf2b4ad5d60ad8694a28ba448c9
lucashour <lucashour1993@gmail.com>
2015-04-06 22:37:16 -0300

    terms#index added

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150317012214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: true do |t|
+  create_table "answers", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "comment_id"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20150317012214) do
 
   add_index "answers", ["comment_id"], name: "index_answers_on_comment_id", using: :btree
 
-  create_table "comments", force: true do |t|
-    t.string   "user_name"
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name",     limit: 255
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -36,24 +36,24 @@ ActiveRecord::Schema.define(version: 20150317012214) do
 
   add_index "comments", ["subject_id"], name: "index_comments_on_subject_id", using: :btree
 
-  create_table "subjects", force: true do |t|
-    t.string   "name"
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",        limit: 255
     t.text     "description"
-    t.string   "picture"
+    t.string   "picture",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "code"
-    t.string   "year"
-    t.string   "int_ord"
-    t.string   "website"
-    t.string   "notes"
+    t.string   "code",        limit: 255
+    t.string   "year",        limit: 255
+    t.string   "int_ord",     limit: 255
+    t.string   "website",     limit: 255
+    t.string   "notes",       limit: 255
   end
 
-  create_table "uploads", force: true do |t|
+  create_table "uploads", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "file_file_name"
-    t.string   "file_content_type"
+    t.string   "file_file_name",    limit: 255
+    t.string   "file_content_type", limit: 255
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
     t.integer  "user_id"
@@ -61,25 +61,25 @@ ActiveRecord::Schema.define(version: 20150317012214) do
 
   add_index "uploads", ["user_id"], name: "index_uploads_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "surname"
-    t.string   "confirmation_token"
+    t.string   "name",                   limit: 255
+    t.string   "surname",                limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
